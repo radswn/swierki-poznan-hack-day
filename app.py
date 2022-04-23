fbfee891b6ee1f2e7ab8014f6d596aad2fb372df
Oskar Szudzik <oszudzik@gmail.com>
2022-04-23 15:16:18 +0200

    Add navbar features

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    if request.method == "GET":
-        cities = ["Poznań", "Kraków", "Warszawa", "Toruń", "Gdańsk", "Gdynia", 
-                    "Sopot", "Szczecin", "Wrocław", "Zakopane"]
-          
-        return render_template("index.html", cities=cities)
+    cities = ["Poznań", "Kraków", "Warszawa", "Toruń", "Gdańsk", "Gdynia", 
+                "Sopot", "Szczecin", "Wrocław", "Zakopane"]
+        
+    return render_template("index.html", cities=cities)
 
 
 @app.route('/search', methods=['POST'])
@@ -18,7 +17,11 @@ def search():
     user_search = request.form['sbar']  
        
     return render_template('plots.html', search=user_search)
-    # return f"<h1>{user_search}</h1>"
+
+
+@app.route('/contact', methods=['GET'])
+def go_contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=getenv('PORT'))
