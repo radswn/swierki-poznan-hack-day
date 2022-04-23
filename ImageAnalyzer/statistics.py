6ebc3c0acdad03c3314b7bce8df933c33e290d01
SebChw <sebastian.chwilczynski@gmail.com>
2022-04-23 12:42:22 +0200

    add inference

---
+++ ImageAnalyzer/statistics.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2 as cv
+
+def normalized_green_level(img: np.ndarray, normalization_factor=1):
+    """Function calculating level of vegetation level in range of [0,1]
+    with the equation 
+
+    veg_level = np.sum(img) / area*normalization_factor 
+
+    Args:
+        img (np.ndarray): array with grayscale image to inference
+        normalization factor (float, optional): . Defaults to 0.5.
+    """
+
+    if len(img.shape) != 1:
+        print("PROVIDE GRAYSCALE IMAGE")
+
+    binarized_img = img > 40
+    cv.imwrite("binarized.jpg", binarized_img * 255)
+    area = binarized_img.shape[0] * binarized_img.shape[1]
+
+    #print(np.sum(binarized_img))
+    #print(area)
+    return np.sum(binarized_img) / (area*normalization_factor)
+
+
+if __name__ == "__main__":
+    img_to_inference = cv.imread("0.jpg")
+    print(type(img_to_inference))
+    print(normalized_green_level(img_to_inference))
--- here/hereApi.py
+++ here/hereApi.py
@@ -8,14 +8,16 @@ class HereApi:
         self.apiKey = getenv("HERE_API_KEY")
         self.URL = 'https://geocode.search.hereapi.com/v1/geocode'
         self.directions = ['west', 'north', 'east', 'south']
-
+    
     def getData(self, location) -> Tuple:
         PARAMS = {'q': location, 'apiKey': self.apiKey}
         r = requests.get(url=self.URL, params=PARAMS)
         data = r.json()
-
+    
         center_latitude = data['items'][0]['position']['lat']
         center_longitude = data['items'][0]['position']['lng']
+        corners = tuple([data['items'][0]['mapView'][x] for x in self.directions])
+        
         location_name = data['items'][0]['title']
         if 'mapView' in data['items'][0].keys():
             corners = tuple([data['items'][0]['mapView'][x] for x in self.directions])
@@ -24,3 +26,9 @@ class HereApi:
                             center_longitude+0.01, center_latitude-0.01])
 
         return location_name, corners
+
+
+if __name__ == '__main__':
+    here = hereApi()
+    print(here.getData("Poznan, Piatkowo"))
+
