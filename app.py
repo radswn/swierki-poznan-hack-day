88bc4fb7b4d2fc9fe554d4556a7da5d898493eda
Oskar Szudzik <oszudzik@gmail.com>
2022-04-23 11:27:33 +0200

    Add searchbar, delete helloworld

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from os import getenv
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "GET":
+        cities = ["Poznań", "Kraków", "Warszawa", "Toruń", "Gdańsk", "Gdynia", 
+                    "Sopot", "Szczecin", "Wrocław", "Zakopane"]
+          
+        return render_template("index.html", cities=cities)
+
 
 
 if __name__ == '__main__':
